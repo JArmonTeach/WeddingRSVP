d7c93811fa61d87e787b3e8dffbe92bdc231d3a7
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-07-28 12:08:25 -0400

    extra guest  section and title added to RSVP form

--- js/RSVP.js
+++ js/RSVP.js
@@ -6,3 +6,18 @@ function attendanceCheck(){
         document.getElementById('food-details').style.display = 'none';
     }
 }
+
+function additionalGuestCheck(){
+    if(document.getElementById('dot-1').checked){
+        document.getElementById('additional-user-details').style.display = 'inherit';
+    }
+    else if(document.getElementById('dot-2').checked){
+        document.getElementById('additional-user-details').style.display = 'inherit';
+    }
+    else if(document.getElementById('dot-3').checked){
+        document.getElementById('additional-user-details').style.display = 'inherit';
+    }
+    else{
+        document.getElementById('additional-user-details').style.display = 'none';
+    }
+}
