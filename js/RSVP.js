a374382081321017a93201dce5e1d006c36ac5c2
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-07-28 23:57:48 -0400

    created clonable div

--- js/RSVP.js
+++ js/RSVP.js
@@ -25,3 +25,13 @@ function additionalGuestCheck(){
 }
 
 //adds input fields for additional guests for their first name, last name and meal option
+function addExtraGuest(){
+    var original = document.getElementById("first-name-box");
+    var clone = original.cloneNode(true);
+    clone.removeAttribute("id");
+    document.getElementById("new-row").appendChild(clone);
+    
+    //TODO:do this for last name and clone the food choices too -- will most likely need to make the first name, last name and food different functions inside this function
+}
+
+//TODO: deleteExtraGuest()
