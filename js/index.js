1995562ad17ee2118963f21b88c163143591af1b
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2022-08-24 18:06:28 -0400

    go to top button added

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-// const navToggle = document.querySelector('.nav-toggle');
-// const navLinks = document.querySelectorAll('.nav__link');
+//Getting the HTML element button
+var topButton = document.getElementById("topBtn");
 
-// navToggle.addEventListener('click', () => {
-//     document.body.classList.toggle('nav-open'); 
-// });
+//calls scrollFunction when scrolls down
+window.onscroll = function(){
+    scrollFunction()
+};
 
-// navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//         document.body.classList.remove('nav-open');
-//     });
-// });
+/**
+ * Function name: scrollFunction
+ * 
+ * Input Parameters: none
+ * 
+ * Description: When user scrolls down 20px from the top of document, the button will show
+ * 
+ * Return value: None
+ */
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        topButton.style.display = "block";
+    }//end if
+    else{
+        topButton.style.display = "none";
+    }//end else
+}//end function
+
+/**
+ * Function name: goToTop
+ * 
+ * Input Parameters: none
+ * 
+ * Description: When the button is clicked, it scrolls back to the top of the document
+ * 
+ * Return value: None
+ */
+ function goToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+ }
